8e30420e212464fc63607e85f03343d59acab7b6
Mahmoud Abukhashaba <74368108+abukhshba@users.noreply.github.com>
2022-12-27 13:44:49 +0200

    Add files via upload

--- coffee/contact.php
+++ coffee/contact.php
@@ -103,7 +103,23 @@ if(empty($errors)){
             </div>
             <?php endforeach; unset($_SESSION['success']); endif; ?>
 
-            <!-- <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30153.788252261566!2d72.82321484621745!3d19.141690214227783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b63aceef0c69%3A0x2aa80cf2287dfa3b!2sJogeshwari%20West%2C%20Mumbai%2C%20Maharashtra%20400047!5e0!3m2!1sen!2sin!4v1629452077891!5m2!1sen!2sin" allowfullscreen="" loading="lazy"></iframe> -->
+            <div style="max-width:100%;overflow:hidden;color:red;width:500px;height:500px;">
+                <div id="my-map-canvas" style="height:100%; width:100%;max-width:100%;"><iframe
+                        style="height:100%;width:100%;border:0;" frameborder="0"
+                        src="https://www.google.com/maps/embed/v1/place?q=EraaSoft,+Mossadak,+street,+Dokki,+Egypt&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
+                </div><a class="google-maps-html" rel="nofollow" href="https://www.bootstrapskins.com/themes"
+                    id="enable-map-data">premium bootstrap themes</a>
+                <style>
+                #my-map-canvas img.text-marker {
+                    max-width: none !important;
+                    background: none !important;
+                }
+
+                img {
+                    max-width: none
+                }
+                </style>
+            </div>
 
             <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
                 <h3>get in touch</h3>
