18c625292f447824244578bbdc327828512a7c0f
Mahmoud Abukhashaba <74368108+abukhshba@users.noreply.github.com>
2022-12-27 20:32:13 +0200

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:abukhshba77@gmail.com">abukhshba77@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>24 hours</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">dokki, Giza</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+      
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Mahmoud Abukhashaba</span> | all rights reserved !</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
